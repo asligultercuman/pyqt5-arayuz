269064d77049f0b1eeea6ae1abdb94b96b6c29e0
Aslıgül Tercuman <161282628+asligultercuman@users.noreply.github.com>
2025-02-28 14:23:55 +0300

    autoscaling fixed for all components
    QVBoxLayout, QHBoxLayout ve QGridLayout kullanıldı. "self" tanımlı kısımlar silindi.

--- mainWindow_3.py
+++ mainWindow_3.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QFrame
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QPushButton, QVBoxLayout, QGridLayout,QHBoxLayout, QFrame
 from PyQt5.QtCore import QTimer, Qt, QDateTime
 from PyQt5.QtGui import QPixmap, QFont
 
@@ -11,22 +11,32 @@ class mainGUI(QWidget):
     def initUI(self):
         self.setWindowTitle("MakinaFleo-HSS")
         self.setGeometry(100, 100, 800, 450)
-        self.setStyleSheet("background-color: lightblue")
+        self.setStyleSheet("background-color: lightblue; border-radius: 20px;")
 
-        self.title_label = QLabel("MakinaFleo-HSS", self)
+        # Main Layout
+        self.main_layout = QVBoxLayout(self)
+        self.setLayout(self.main_layout)
+        
+        # Upper Layout (Title and Logo)
+        self.top_layout = QHBoxLayout()
+        self.main_layout.addLayout(self.top_layout)
+
+        # Title (Upper Left)
+        self.title_label = QLabel("MakinaFleo-HSS")
         self.title_label.setStyleSheet("color: red; font-size: 14px;")
-        self.title_label.move(10, 10)
+        self.top_layout.addWidget(self.title_label, alignment=Qt.AlignLeft | Qt.AlignTop)
 
         # Upper right logo
-        #self.logo_label = QLabel(self)
-        #self.logo_label.setPixmap(QPixmap("logo.png"))  # Resmi ekle
-        #self.logo_label.setScaledContents(True)
-        #self.logo_label.setGeometry(670, 20, 100, 100)
+        self.logo_label = QLabel()
+        self.logo_label.setPixmap(QPixmap("logo.png"))
+        self.logo_label.setScaledContents(True)
+        self.logo_label.setFixedSize(80,80)
+        self.top_layout.addWidget(self.logo_label, alignment=Qt.AlignRight | Qt.AlignTop)
 
         # Ortadaki Video Alanı (Gri Arkaplan)
-        self.video_frame = QLabel(self)
-        self.video_frame.setGeometry(100, 50, 600, 300)
+        self.video_frame = QLabel()
         self.video_frame.setStyleSheet("background-color: lightgray; border: 1px solid black;")
+        self.main_layout.addWidget(self.video_frame, stretch=1)
 
 # Subclass QMainWindow to customize your application's main window
 #class MainWindow(QMainWindow):
